7083b16b8d5f752326ef822739c4be91b5bffc5f
William Hart <william.hart@maths.ox.ac.uk>
2024-05-14 18:42:53 +0100

    Use snakemake to control inputs

--- scripts/sensitivity_unvacc_r.py
+++ scripts/sensitivity_unvacc_r.py
@@ -1,40 +1,34 @@
-import copy
-import itertools
 import pathlib
 import sys
 
+# import snakemake
+
 sys.path.insert(1, str(pathlib.Path(__file__).parents[1]))
 
-import joblib
-import numpy as np
-import pandas as pd
-import tqdm
+from scripts import optimizing_vaccination
 
-from covidboosters import OutbreakRiskModel
-from scripts import optimizing_vaccination, vaccination_example
-from scripts.default_parameters import get_default_parameters
+unvacc_rep_no_mean_vals = [2, 2.5, 3]
+unvacc_rep_no_prop_var_vals = [0.1, 0.2, 0.3]
 
+unvacc_rep_no_mean = unvacc_rep_no_mean_vals[
+    int(snakemake.wildcards["mean_index"])  # noqa: F821
+]
+unvacc_rep_no_prop_var = unvacc_rep_no_prop_var_vals[
+    int(snakemake.wildcards["prop_var_index"])  # noqa: F821
+]
+save_path = snakemake.output[0]  # noqa: F821
+save_path_best = snakemake.output[1]  # noqa: F821
+save_path_vaccination_time_range_best = snakemake.output[2]  # noqa: F821
 
-def run_analyses():
-    unvacc_rep_no_mean_vals = [1, 2, 3]
-    unvacc_rep_no_prop_var_vals = [0.1, 0.25, 0.4]
-    results_dir = pathlib.Path(__file__).parents[1] / "results/sensitivity_unvacc_r"
-    results_dir.mkdir(exist_ok=True, parents=True)
-    for unvacc_rep_no_mean, unvacc_rep_no_prop_var in itertools.product(
-        unvacc_rep_no_mean_vals, unvacc_rep_no_prop_var_vals
-    ):
-        file_ext = f"_{unvacc_rep_no_mean}_{100*unvacc_rep_no_prop_var}"
-        optimizing_vaccination.run_analyses(
-            save_path=results_dir / f"grid_search{file_ext}.csv",
-            save_path_best=results_dir / f"best{file_ext}.csv",
-            save_path_vaccination_time_range_best=results_dir
-            / f"vaccination_time_range_best{file_ext}.csv",
-            load_path_susceptibility_all_0=pathlib.Path(__file__).parents[1]
-            / "results/susceptibility_all_0.csv",
-            unvaccinated_reproduction_no_mean=unvacc_rep_no_mean,
-            unvaccinated_reproduction_no_prop_variation=unvacc_rep_no_prop_var,
-        )
+results_dir = pathlib.Path(__file__).parents[1] / "results/sensitivity_unvacc_r"
+results_dir.mkdir(exist_ok=True, parents=True)
 
-
-if __name__ == "__main__":
-    run_analyses()
+optimizing_vaccination.run_analyses(
+    save_path=save_path,
+    save_path_best=save_path_best,
+    save_path_vaccination_time_range_best=save_path_vaccination_time_range_best,
+    load_path_susceptibility_all_0=pathlib.Path(__file__).parents[1]
+    / "results/susceptibility_all_0.csv",
+    unvaccinated_reproduction_no_mean=unvacc_rep_no_mean,
+    unvaccinated_reproduction_no_prop_variation=unvacc_rep_no_prop_var,
+)
