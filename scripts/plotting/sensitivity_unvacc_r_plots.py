287e292e5f68fd85a48c7abec5e986c8efd33022
William Hart <william.hart@maths.ox.ac.uk>
2024-05-16 14:28:22 +0100

    Reformat to allow running in and out of snakemake

--- scripts/plotting/sensitivity_unvacc_r_plots.py
+++ scripts/plotting/sensitivity_unvacc_r_plots.py
@@ -1,23 +1,44 @@
+import itertools
 import pathlib
 import sys
+
+import matplotlib.pyplot as plt
 
 sys.path.insert(1, str(pathlib.Path(__file__).parents[2]))
 
 from scripts.plotting import optimizing_vaccination_plots
 
-load_path = snakemake.input[2]  # noqa: F821
-load_path_best = snakemake.input[3]  # noqa: F821
-load_path_vaccination_time_range_best = snakemake.input[4]  # noqa: F821
-figure_path_heatmap = snakemake.output[0]  # noqa: F821
-figure_path_best = snakemake.output[1]  # noqa: F821
 
-figure_dir = pathlib.Path(__file__).parents[2] / "figures/optimizing_vaccination"
-figure_dir.mkdir(exist_ok=True, parents=True)
+def make_plots(mean_index, prop_var_index):
+    results_dir = pathlib.Path(__file__).parents[2] / "results/sensitivity_unvacc_r"
+    figure_dir = pathlib.Path(__file__).parents[2] / "figures/sensitivity_unvacc_r"
+    figure_dir.mkdir(exist_ok=True, parents=True)
 
-optimizing_vaccination_plots.make_plots(
-    load_path=load_path,
-    load_path_best=load_path_best,
-    load_path_vaccination_time_range_best=load_path_vaccination_time_range_best,
-    figure_path_heatmap=figure_path_heatmap,
-    figure_path_best=figure_path_best,
-)
+    load_path = results_dir / f"grid_search_{mean_index}_{prop_var_index}.csv"
+    load_path_best = results_dir / f"best_{mean_index}_{prop_var_index}.csv"
+    load_path_vaccination_time_range_best = (
+        results_dir / f"vaccination_time_range_best_{mean_index}_{prop_var_index}.csv"
+    )
+    figure_path_heatmap = figure_dir / f"heatmap_{mean_index}_{prop_var_index}.svg"
+    figure_path_best = figure_dir / f"best_{mean_index}_{prop_var_index}.svg"
+
+    optimizing_vaccination_plots.make_plots(
+        load_path=load_path,
+        load_path_best=load_path_best,
+        load_path_vaccination_time_range_best=load_path_vaccination_time_range_best,
+        figure_path_heatmap=figure_path_heatmap,
+        figure_path_best=figure_path_best,
+        show_plots=False,
+    )
+
+
+if __name__ == "__main__":
+    if "snakemake" in globals() and "mean_index" in snakemake.wildcards:  # noqa: F821
+        make_plots(
+            mean_index=int(snakemake.wildcards["mean_index"]),  # noqa: F821
+            prop_var_index=int(snakemake.wildcards["prop_var_index"]),  # noqa: F821
+        )
+    else:
+        for _mean_index, _prop_var_index in itertools.product(range(3), range(3)):
+            make_plots(_mean_index, _prop_var_index)
+        plt.show()
--- scripts/sensitivity_unvacc_r.py
+++ scripts/sensitivity_unvacc_r.py
@@ -1,3 +1,4 @@
+import itertools
 import pathlib
 import sys
 
@@ -10,25 +11,38 @@ from scripts import optimizing_vaccination
 unvacc_rep_no_mean_vals = [2, 2.5, 3]
 unvacc_rep_no_prop_var_vals = [0.1, 0.2, 0.3]
 
-unvacc_rep_no_mean = unvacc_rep_no_mean_vals[
-    int(snakemake.wildcards["mean_index"])  # noqa: F821
-]
-unvacc_rep_no_prop_var = unvacc_rep_no_prop_var_vals[
-    int(snakemake.wildcards["prop_var_index"])  # noqa: F821
-]
-save_path = snakemake.output[0]  # noqa: F821
-save_path_best = snakemake.output[1]  # noqa: F821
-save_path_vaccination_time_range_best = snakemake.output[2]  # noqa: F821
 
-results_dir = pathlib.Path(__file__).parents[1] / "results/sensitivity_unvacc_r"
-results_dir.mkdir(exist_ok=True, parents=True)
+def run_analyses(mean_index, prop_var_index):
+    results_dir = pathlib.Path(__file__).parents[1] / "results/sensitivity_unvacc_r"
+    results_dir.mkdir(exist_ok=True, parents=True)
+    load_path_susceptibility_all_0 = (
+        pathlib.Path(__file__).parents[1] / "results/susceptibility_all_0.csv"
+    )
+    save_path = results_dir / f"grid_search_{mean_index}_{prop_var_index}.csv"
+    save_path_best = results_dir / f"best_{mean_index}_{prop_var_index}.csv"
+    save_path_vaccination_time_range_best = (
+        results_dir / f"vaccination_time_range_best_{mean_index}_{prop_var_index}.csv"
+    )
+    optimizing_vaccination.run_analyses(
+        save_path=save_path,
+        save_path_best=save_path_best,
+        save_path_vaccination_time_range_best=save_path_vaccination_time_range_best,
+        load_path_susceptibility_all_0=load_path_susceptibility_all_0,
+        unvaccinated_reproduction_no_mean=unvacc_rep_no_mean_vals[mean_index],
+        unvaccinated_reproduction_no_prop_variation=unvacc_rep_no_prop_var_vals[
+            prop_var_index
+        ],
+    )
 
-optimizing_vaccination.run_analyses(
-    save_path=save_path,
-    save_path_best=save_path_best,
-    save_path_vaccination_time_range_best=save_path_vaccination_time_range_best,
-    load_path_susceptibility_all_0=pathlib.Path(__file__).parents[1]
-    / "results/susceptibility_all_0.csv",
-    unvaccinated_reproduction_no_mean=unvacc_rep_no_mean,
-    unvaccinated_reproduction_no_prop_variation=unvacc_rep_no_prop_var,
-)
+
+if __name__ == "__main__":
+    if "snakemake" in globals() and "mean_index" in snakemake.wildcards:  # noqa: F821
+        run_analyses(
+            mean_index=int(snakemake.wildcards["mean_index"]),  # noqa: F821
+            prop_var_index=int(snakemake.wildcards["prop_var_index"]),  # noqa: F821
+        )
+    else:
+        for _mean_index, _prop_var_index in itertools.product(
+            range(len(unvacc_rep_no_mean_vals)), range(len(unvacc_rep_no_prop_var_vals))
+        ):
+            run_analyses(mean_index=_mean_index, prop_var_index=_prop_var_index)
