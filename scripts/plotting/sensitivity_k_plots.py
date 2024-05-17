53d033dc301f321dc1d94b69d0310e09d6cd3a86
William Hart <william.hart@maths.ox.ac.uk>
2024-05-17 18:09:54 +0100

    Remake panels for default parameters with different y limits (matching limits for panels for parameters considered in sensitivity analysis)

--- scripts/plotting/sensitivity_k_plots.py
+++ scripts/plotting/sensitivity_k_plots.py
@@ -9,15 +9,24 @@ from scripts.plotting import optimizing_vaccination_plots
 
 
 def make_plots(k_index):
-    results_dir = pathlib.Path(__file__).parents[2] / "results/sensitivity_k"
     figure_dir = pathlib.Path(__file__).parents[2] / "figures/sensitivity_k"
     figure_dir.mkdir(exist_ok=True, parents=True)
-
-    load_path = results_dir / f"grid_search_{k_index}.csv"
-    load_path_best = results_dir / f"best_{k_index}.csv"
-    load_path_vaccination_time_range_best = (
-        results_dir / f"vaccination_time_range_best_{k_index}.csv"
-    )
+    if k_index == "default":
+        results_dir = (
+            pathlib.Path(__file__).parents[2] / "results/optimizing_vaccination"
+        )
+        load_path = results_dir / "grid_search.csv"
+        load_path_best = results_dir / "best.csv"
+        load_path_vaccination_time_range_best = (
+            results_dir / "vaccination_time_range_best.csv"
+        )
+    else:
+        results_dir = pathlib.Path(__file__).parents[2] / "results/sensitivity_k"
+        load_path = results_dir / f"grid_search_{k_index}.csv"
+        load_path_best = results_dir / f"best_{k_index}.csv"
+        load_path_vaccination_time_range_best = (
+            results_dir / f"vaccination_time_range_best_{k_index}.csv"
+        )
     figure_path_heatmap = figure_dir / f"heatmap_{k_index}.svg"
     figure_path_best = figure_dir / f"best_{k_index}.svg"
 
@@ -28,15 +37,17 @@ def make_plots(k_index):
         figure_path_heatmap=figure_path_heatmap,
         figure_path_best=figure_path_best,
         show_plots=False,
+        ylim_best=(0, 0.6),
     )
 
 
 if __name__ == "__main__":
-    if "snakemake" in globals() and "mean_index" in snakemake.wildcards:  # noqa: F821
-        make_plots(
-            k_index=int(snakemake.wildcards["k_index"]),  # noqa: F821
-        )
+    if "snakemake" in globals():
+        _k_index = snakemake.wildcards["k_index"]  # noqa: F821
+        if _k_index != "default":
+            _k_index = int(_k_index)
+        make_plots(k_index=_k_index)
     else:
-        for _k_index in range(3):
+        for _k_index in ["default", 0, 1]:
             make_plots(_k_index)
         plt.show()
