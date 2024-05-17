52e44708e2e3081f6cdb15e04e0d7c3e0eab256b
William Hart <william.hart@maths.ox.ac.uk>
2024-05-17 18:04:45 +0100

    Don't rerun defaults

--- scripts/sensitivity_k.py
+++ scripts/sensitivity_k.py
@@ -1,26 +1,25 @@
-import itertools
 import pathlib
 import sys
 
-# import snakemake
+import numpy as np
 
 sys.path.insert(1, str(pathlib.Path(__file__).parents[1]))
 
 from scripts import optimizing_vaccination
 
-k_vals = [0.23, 0.41, 0.60]  # based on CI in https://doi.org/10.1186/s12889-023-15915-1
+k_vals = [0.23, 0.60]  # based on CI in https://doi.org/10.1186/s12889-023-15915-1
 
 
 def run_analyses(k_index):
     results_dir = pathlib.Path(__file__).parents[1] / "results/sensitivity_k"
     results_dir.mkdir(exist_ok=True, parents=True)
-    load_path_susceptibility_all_0 = (
-        pathlib.Path(__file__).parents[1] / "results/susceptibility_all_0.csv"
-    )
     save_path = results_dir / f"grid_search_{k_index}.csv"
     save_path_best = results_dir / f"best_{k_index}.csv"
     save_path_vaccination_time_range_best = (
         results_dir / f"vaccination_time_range_best_{k_index}.csv"
+    )
+    load_path_susceptibility_all_0 = (
+        pathlib.Path(__file__).parents[1] / "results/susceptibility_all_0.csv"
     )
     optimizing_vaccination.run_analyses(
         save_path=save_path,
@@ -32,7 +31,7 @@ def run_analyses(k_index):
 
 
 if __name__ == "__main__":
-    if "snakemake" in globals() and "mean_index" in snakemake.wildcards:  # noqa: F821
+    if "snakemake" in globals():
         run_analyses(
             k_index=int(snakemake.wildcards["k_index"]),  # noqa: F821
         )
