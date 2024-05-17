d93c512cc0f6427a35958ca94689e482d623cac8
William Hart <william.hart@maths.ox.ac.uk>
2024-05-17 18:07:59 +0100

    Add analysis of within-host dynamics following vaccination

---
+++ scripts/plotting/within_host_dynamics_plots.py
@@ -0,0 +1,64 @@
+import pathlib
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+sys.path.insert(1, str(pathlib.Path(__file__).parents[2]))
+
+from scripts.default_parameters import get_default_parameters
+from scripts.plotting import plotting_utils
+
+
+def make_plots(plot_ci=True):
+    plotting_utils.set_sns_theme()
+    results_dir = pathlib.Path(__file__).parents[2] / "results"
+    figure_dir = pathlib.Path(__file__).parents[2] / "figures/within_host_dynamics"
+    figure_dir.mkdir(exist_ok=True, parents=True)
+    # Load the results
+    df = pd.read_csv(results_dir / "within_host_dynamics.csv", index_col="time")
+    default_parameters = get_default_parameters()
+    period = default_parameters["period"]
+    # Plot antibody dynamics following vaccination
+    _, ax = plotting_utils.setup_figure()
+    df["antibodies_mean"].plot(ax=ax)
+    ax.set_xlim(0, period)
+    ax.set_xticks(np.arange(0, period + 1, period / 6))
+    ax.set_ylim(0, 4000)
+    if plot_ci:
+        ax.fill_between(
+            df.index,
+            df["antibodies_95ci_lower"],
+            df["antibodies_95ci_upper"],
+            alpha=0.5,
+        )
+        ax.set_ylim(0, 14000)
+    ax.set_xlabel("Time since vaccination (days)")
+    ax.set_ylabel("Antibody titre (AU)")
+    plt.savefig(figure_dir / "antibodies.pdf")
+    plt.savefig(figure_dir / "antibodies.svg")
+    # Plot susceptibility dynamics following vaccination
+    _, ax = plotting_utils.setup_figure()
+    df["susceptibility_mean"].plot(ax=ax)
+    ax.set_xlim(0, period)
+    ax.set_xticks(np.arange(0, period + 1, period / 6))
+    ax.set_ylim(0, 1)
+    if plot_ci:
+        ax.fill_between(
+            df.index,
+            df["susceptibility_95ci_lower"],
+            df["susceptibility_95ci_upper"],
+            alpha=0.5,
+        )
+        ax.set_xlabel("Time since vaccination (days)")
+    ax.set_ylabel("Relative susceptibility")
+    plt.savefig(figure_dir / "susceptibility.pdf")
+    plt.savefig(figure_dir / "susceptibility.svg")
+    # Show plots
+    if "snakemake" not in globals():
+        plt.show()
+
+
+if __name__ == "__main__":
+    make_plots(plot_ci=True)
---
+++ scripts/within_host_dynamics.py
@@ -0,0 +1,66 @@
+import pathlib
+import sys
+
+import numpy as np
+import pandas as pd
+
+sys.path.insert(1, str(pathlib.Path(__file__).parents[1]))
+
+from covidboosters import IndividualSusceptibilityModel
+from scripts.default_parameters import get_default_parameters
+
+
+def run_analyses(save_path, **kwargs_individual_susceptibility_model_in):
+    default_parameters = get_default_parameters()
+    period = default_parameters["period"]
+    population_size = default_parameters["population_size"]
+    antibody_model_params_pop = default_parameters["antibody_model_params_pop"]
+    antibody_model_params_random_effects = default_parameters[
+        "antibody_model_params_random_effects"
+    ]
+    rng = np.random.default_rng(seed=2)
+    time_vec = np.arange(period)
+    antibodies_mat = np.zeros((period, population_size))
+    susceptibility_mat = np.zeros((period, population_size))
+    for individual_no in range(population_size):
+        antibody_model_params = {}
+        for param_name, param_pop in antibody_model_params_pop.items():
+            param_random_effect = antibody_model_params_random_effects[param_name]
+            antibody_model_params[param_name] = param_pop * np.exp(
+                param_random_effect * rng.standard_normal()
+            )
+        kwargs_individual_susceptibility_model = {
+            "susceptibility_func_params": default_parameters[
+                "susceptibility_func_params"
+            ],
+            "vaccination_times": 0,
+            **kwargs_individual_susceptibility_model_in,
+            "antibody_model_params": antibody_model_params,
+        }
+        individual_susceptibility_model = IndividualSusceptibilityModel(
+            **kwargs_individual_susceptibility_model
+        )
+        antibodies_mat[:, individual_no] = individual_susceptibility_model.antibodies(
+            time_vec
+        )
+        susceptibility_mat[:, individual_no] = (
+            individual_susceptibility_model.susceptibility(time_vec)
+        )
+    df = pd.DataFrame({"time": time_vec})
+    df.set_index("time", inplace=True)
+    df["antibodies_mean"] = np.mean(antibodies_mat, axis=1)
+    df["antibodies_median"] = np.median(antibodies_mat, axis=1)
+    df["antibodies_95ci_lower"] = np.percentile(antibodies_mat, 2.5, axis=1)
+    df["antibodies_95ci_upper"] = np.percentile(antibodies_mat, 97.5, axis=1)
+    df["susceptibility_mean"] = np.mean(susceptibility_mat, axis=1)
+    df["susceptibility_median"] = np.median(susceptibility_mat, axis=1)
+    df["susceptibility_95ci_lower"] = np.percentile(susceptibility_mat, 2.5, axis=1)
+    df["susceptibility_95ci_upper"] = np.percentile(susceptibility_mat, 97.5, axis=1)
+    # Save the results
+    df.to_csv(save_path)
+
+
+if __name__ == "__main__":
+    results_dir = pathlib.Path(__file__).parents[1] / "results"
+    results_dir.mkdir(exist_ok=True, parents=True)
+    run_analyses(save_path=results_dir / "within_host_dynamics.csv")
