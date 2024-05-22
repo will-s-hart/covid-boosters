a1bc921e082b03b2522ab43472ec809bd256fb61
William Hart <william.hart@maths.ox.ac.uk>
2024-05-22 17:49:28 +0100

    Update defaults

--- scripts/default_parameters.py
+++ scripts/default_parameters.py
@@ -13,7 +13,7 @@ def get_default_parameters():
     period = 360
     unvacc_rep_no_mean = 2.5  # with below, max is 3
     unvacc_rep_no_prop_var = 0.2
-    peak_transmission_time = 1
+    peak_transmission_time = 0
 
     generation_time_logmean = 0.979396480343543
     generation_time_logsd = 0.470500197316974
@@ -50,7 +50,7 @@ def get_default_parameters():
     }
 
     vaccination_time_range = [270, 345]
-    proportion_vaccinated = 1 / 3
+    proportion_vaccinated = 0.35
     population_size = 1000
 
     sim_incidence_cutoff = 10
