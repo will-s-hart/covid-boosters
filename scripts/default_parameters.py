b37a0086624408bd5b7a353a0977bec30de8a62f
William Hart <william.hart@maths.ox.ac.uk>
2024-05-29 18:10:38 +0100

    Update defaults

--- scripts/default_parameters.py
+++ scripts/default_parameters.py
@@ -32,25 +32,13 @@ def get_default_parameters():
         "random_effect"
     ].to_dict()
 
-    # antibody_covalescent = 114.92
-    # half_protection_neutralizing_ab = 0.2
-    # omicron_reduction_factor = 22
-    # # vaccine_adaptation_factor = 1.61
-    # vaccine_adaptation_factor = 1
-
-    # susceptibility_func_params = {
-    #     "antibody_response_steepness": np.exp(1.13) / np.log(10),
-    #     "half_protection_antibody": half_protection_neutralizing_ab
-    #     * antibody_covalescent
-    #     * (omicron_reduction_factor / vaccine_adaptation_factor),
-    # }
     susceptibility_func_params = {
         "antibody_response_steepness": 3.1 / np.log(10),
         "half_protection_antibody": 1000,
     }
 
     vaccination_time_range = [270, 345]
-    proportion_vaccinated = 0.35
+    proportion_vaccinated = 0.6
     population_size = 1000
 
     sim_incidence_cutoff = 10
