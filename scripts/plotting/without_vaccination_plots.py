843622e4a3492b80ccc0c3a76fcb66b761c28466
William Hart <william.hart@maths.ox.ac.uk>
2024-06-19 11:01:24 +0100

    Update analyses

--- scripts/plotting/without_vaccination_plots.py
+++ scripts/plotting/without_vaccination_plots.py
@@ -3,6 +3,7 @@ import sys
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 sys.path.insert(1, str(pathlib.Path(__file__).parents[2]))
 
@@ -15,49 +16,37 @@ def make_plots():
     results_dir = pathlib.Path(__file__).parents[2] / "results/without_vaccination"
     figure_dir = pathlib.Path(__file__).parents[2] / "figures/without_vaccination"
     figure_dir.mkdir(exist_ok=True, parents=True)
+    colors = [sns.color_palette()[i] for i in [2, 0, 1, 3, 7]]
     # Load the results
-    outbreak_risk_df_methods = pd.read_csv(
-        results_dir / "methods.csv", index_col="time"
+    df_reproduction_number = pd.read_csv(
+        results_dir / "reproduction_number.csv", index_col="time"
     )
-    outbreak_risk_df_dispersion = pd.read_csv(
-        results_dir / "dispersion.csv", index_col="time"
-    )
+    df_analytic = pd.read_csv(results_dir / "analytic.csv", index_col="time")
+    df_simulated = pd.read_csv(results_dir / "simulated.csv", index_col="time")
     period = get_default_parameters()["period"]
     # Plot instantaneous reproduction number
     _, ax = plotting_utils.setup_figure()
-    outbreak_risk_df_methods["r"].plot(ax=ax)
+    df_reproduction_number["r"].plot(ax=ax)
     plotting_utils.months_x_axis(ax, period=period, no_periods=2)
     ax.set_ylim(0, 3.02)
     ax.set_ylabel("Instantaneous reproduction number")
     plt.savefig(figure_dir / "reproduction_number.pdf")
     plt.savefig(figure_dir / "reproduction_number.svg")
-    # Plot comparison of COR, IOR, and SOR for a single example with no vaccination
+    # Plot comparison of COR/SOR values for different values of the dispersion parameter
     _, ax = plotting_utils.setup_figure()
-    outbreak_risk_df_methods["cor"].plot(ax=ax, label="Case outbreak risk")
-    outbreak_risk_df_methods["ior"].plot(
-        style="--", ax=ax, label="Instantaneous outbreak risk"
-    )
-    outbreak_risk_df_methods["sor"].plot(
-        style="yo", ax=ax, label="Simulated outbreak risk"
-    )
-    plotting_utils.months_x_axis(ax, period=period, no_periods=2)
-    ax.set_ylim(0, 1)
-    ax.set_ylabel("Outbreak risk")
-    ax.legend()
-    plt.savefig(figure_dir / "outbreak_risk_methods.pdf")
-    plt.savefig(figure_dir / "outbreak_risk_methods.svg")
-    # Plot comparison of COR values for different values of the dispersion parameter
-    _, ax = plotting_utils.setup_figure()
-    for dispersion_param in outbreak_risk_df_dispersion.columns:
-        outbreak_risk_df_dispersion[dispersion_param].plot(
-            label="$\\it{k}$ = " + dispersion_param
+    for dispersion_param, color in zip(df_analytic.columns, colors):
+        df_analytic[dispersion_param].plot(
+            color=color, label="$\\it{k}$ = " + dispersion_param
+        )
+        df_simulated[dispersion_param].plot(
+            color=color, style="o", markersize=5, alpha=0.5, label=""
         )
     plotting_utils.months_x_axis(ax, period=period, no_periods=2)
     ax.set_ylim(0, 1)
-    ax.set_ylabel("Case outbreak risk")
+    ax.set_ylabel("Outbreak risk")
     ax.legend(loc="upper right")
-    plt.savefig(figure_dir / "outbreak_risk_dispersion.pdf")
-    plt.savefig(figure_dir / "outbreak_risk_dispersion.svg")
+    plt.savefig(figure_dir / "outbreak_risk.pdf")
+    plt.savefig(figure_dir / "outbreak_risk.svg")
     # Show plots
     if "snakemake" not in globals():
         plt.show()
--- scripts/without_vaccination.py
+++ scripts/without_vaccination.py
@@ -14,53 +14,50 @@ def run_analyses():
     default_parameters = get_default_parameters()
     period = default_parameters["period"]
     time_vec = np.arange(2 * period)
-    # Comparison of COR, IOR, and SOR for a single example with no vaccination
-    kwargs_outbreak_risk_model_methods = {
+    time_vec_sor = np.arange(2 * period, step=30)
+    kwargs_outbreak_risk_model = {
         "period": default_parameters["period"],
         "peak_transmission_time": default_parameters["peak_transmission_time"],
         "generation_time_dist": default_parameters["generation_time_dist"],
         "unvaccinated_reproduction_no_mean": 2,
         "unvaccinated_reproduction_no_prop_variation": 0.5,
-        "dispersion_param": 0.5,
         "rng_seed": 2,
     }
-    outbreak_risk_model_methods = OutbreakRiskModel(
-        **kwargs_outbreak_risk_model_methods
-    )
-    df_methods = pd.DataFrame({"time": time_vec})
-    df_methods.set_index("time", inplace=True)
-    df_methods["r"] = outbreak_risk_model_methods.reproduction_no(time_vec)
-    df_methods["cor"] = outbreak_risk_model_methods.case_outbreak_risk(time_vec)
-    df_methods["ior"] = outbreak_risk_model_methods.instantaneous_outbreak_risk(
-        time_vec
-    )
-    time_vec_sor = np.arange(2 * period, step=30)
     kwargs_sor = {
         "incidence_cutoff": default_parameters["sim_incidence_cutoff"],
         "no_simulations": default_parameters["no_simulations"],
     }
-    df_methods.loc[time_vec_sor, "sor"] = (
-        outbreak_risk_model_methods.simulated_outbreak_risk(time_vec_sor, **kwargs_sor)
-    )
-    # Comparison of COR values for different values of the dispersion parameter
-    dispersion_param_vals = [0.1, 0.5, 1, 10, 100]
-    df_dispersion = pd.DataFrame({"time": time_vec})
-    df_dispersion.set_index("time", inplace=True)
+    # Reproduction number over time
+    df_reproduction_number = pd.DataFrame({"time": time_vec})
+    df_reproduction_number.set_index("time", inplace=True)
+    df_reproduction_number["r"] = OutbreakRiskModel(
+        dispersion_param=1, **kwargs_outbreak_risk_model
+    ).reproduction_no(time_vec)
+    # Comparison of COR/SOR values for different values of the dispersion parameter
+    dispersion_param_vals = [0.1, default_parameters["dispersion_param"], 1, 10, 100]
+    df_analytic = pd.DataFrame({"time": time_vec})
+    df_analytic.set_index("time", inplace=True)
+    df_simulated = pd.DataFrame({"time": time_vec_sor})
+    df_simulated.set_index("time", inplace=True)
     for dispersion_param in dispersion_param_vals:
+        print(f"Dispersion parameter: {dispersion_param}")
         outbreak_risk_model_curr = OutbreakRiskModel(
-            **{
-                **kwargs_outbreak_risk_model_methods,
-                "dispersion_param": dispersion_param,
-            }
+            dispersion_param=dispersion_param,
+            **kwargs_outbreak_risk_model,
         )
-        df_dispersion[f"{dispersion_param}"] = (
+        df_analytic[f"{dispersion_param}"] = (
             outbreak_risk_model_curr.case_outbreak_risk(time_vec)
+        )
+        print(f"Running {kwargs_sor['no_simulations']} simulations...")
+        df_simulated[f"{dispersion_param}"] = (
+            outbreak_risk_model_curr.simulated_outbreak_risk(time_vec_sor, **kwargs_sor)
         )
     # Save the results
     results_dir = pathlib.Path(__file__).parents[1] / "results/without_vaccination"
     results_dir.mkdir(exist_ok=True, parents=True)
-    df_methods.to_csv(results_dir / "methods.csv")
-    df_dispersion.to_csv(results_dir / "dispersion.csv")
+    df_reproduction_number.to_csv(results_dir / "reproduction_number.csv")
+    df_analytic.to_csv(results_dir / "analytic.csv")
+    df_simulated.to_csv(results_dir / "simulated.csv")
 
 
 if __name__ == "__main__":
