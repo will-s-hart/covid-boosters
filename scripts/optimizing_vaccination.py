a6cce14f2be5150ddd4b8669b476aa1e5ad4d9cd
William Hart <william.hart@maths.ox.ac.uk>
2024-06-19 14:07:26 +0100

    Add in optional refinement to find optimum start/duration without comprehensive grid search

--- scripts/optimizing_vaccination.py
+++ scripts/optimizing_vaccination.py
@@ -16,11 +16,13 @@ from scripts.default_parameters import get_default_parameters
 
 
 def run_analyses(
-    save_path,
+    save_path_grid_search=None,
     save_path_best=None,
     save_path_vaccination_time_range_best=None,
     load_path_susceptibility_all_0=None,
     obj_func=np.max,
+    grid_step=10,
+    refine=True,
     **kwargs_outbreak_risk_model,
 ):
     default_parameters = get_default_parameters()
@@ -61,54 +63,35 @@ def run_analyses(
         )
         _ = outbreak_risk_model.susceptibility(np.arange(period))
     # Grid of vaccination start times and durations
-    step = 10
-    vaccination_start_time_vals = np.arange(0, period + 1, step)
-    vaccination_duration_vals = np.arange(step, period + 1, step)
-
-    # Loop over vaccination start times and durations
-    def worker(vaccination_start_time, vaccination_duration):
-        vaccination_time_range = [
-            vaccination_start_time,
-            vaccination_start_time + vaccination_duration,
+    start_time_grid_vals = np.arange(0, period + 1, grid_step)
+    duration_grid_vals = np.arange(grid_step, period + 1, grid_step)
+    start_time_best, duration_best = _run_grid_search(
+        outbreak_risk_model,
+        save_path_grid_search=save_path_grid_search,
+        obj_func=obj_func,
+        start_time_grid_vals=start_time_grid_vals,
+        duration_grid_vals=duration_grid_vals,
+    )
+    # Refine the search
+    if refine and grid_step > 1:
+        start_time_grid_vals_refine = np.arange(
+            start_time_best - grid_step, start_time_best + grid_step + 1
+        )
+        start_time_grid_vals_refine = start_time_grid_vals_refine[
+            (start_time_grid_vals_refine >= 0) & (start_time_grid_vals_refine <= period)
         ]
-        outbreak_risk_model_curr = copy.deepcopy(outbreak_risk_model)
-        outbreak_risk_model_curr.update_vaccination_params(
-            vaccination_time_range=vaccination_time_range
+        duration_grid_vals_refine = np.arange(
+            duration_best - grid_step, duration_best + grid_step + 1
         )
-        cor_vec = outbreak_risk_model_curr.case_outbreak_risk(np.arange(period))
-        obj_val = obj_func(cor_vec)
-        return obj_val
-
-    n_jobs = joblib.cpu_count(only_physical_cores=True)
-    print(f"Scanning over start times and durations using {n_jobs} cores")
-    results = list(
-        tqdm.tqdm(
-            joblib.Parallel(return_as="generator", n_jobs=n_jobs)(
-                joblib.delayed(worker)(start, duration)
-                for start, duration in itertools.product(
-                    vaccination_start_time_vals, vaccination_duration_vals
-                )
-            ),
-            total=len(vaccination_start_time_vals) * len(vaccination_duration_vals),
+        duration_grid_vals_refine = duration_grid_vals_refine[
+            (duration_grid_vals_refine >= 1) & (duration_grid_vals_refine <= period)
+        ]
+        start_time_best, duration_best = _run_grid_search(
+            outbreak_risk_model,
+            obj_func=obj_func,
+            start_time_grid_vals=start_time_grid_vals_refine,
+            duration_grid_vals=duration_grid_vals_refine,
         )
-    )
-    df_grid_search = (
-        pd.DataFrame(
-            np.reshape(
-                results,
-                (len(vaccination_start_time_vals), len(vaccination_duration_vals)),
-            ),
-            index=vaccination_start_time_vals,
-            columns=vaccination_duration_vals,
-            dtype=float,
-        )
-        .rename_axis("start", axis=0)
-        .rename_axis("duration", axis=1)
-    )
-    # Save the results
-    df_grid_search.to_csv(save_path)
-    # Calculate optimal vaccination time range and corresponding case outbreak risk
-    start_time_best, duration_best = df_grid_search.stack().idxmin()
     vaccination_time_range_best = [
         start_time_best,
         start_time_best + duration_best,
@@ -131,14 +114,73 @@ def run_analyses(
         ).to_csv(save_path_vaccination_time_range_best)
 
 
+def _run_grid_search(
+    outbreak_risk_model,
+    save_path_grid_search=None,
+    obj_func=None,
+    start_time_grid_vals=None,
+    duration_grid_vals=None,
+):
+    # Define objective function
+    period = outbreak_risk_model._period
+
+    def worker(vaccination_start_time, vaccination_duration):
+        vaccination_time_range = [
+            vaccination_start_time,
+            vaccination_start_time + vaccination_duration,
+        ]
+        outbreak_risk_model_curr = copy.deepcopy(outbreak_risk_model)
+        outbreak_risk_model_curr.update_vaccination_params(
+            vaccination_time_range=vaccination_time_range
+        )
+        cor_vec = outbreak_risk_model_curr.case_outbreak_risk(np.arange(period))
+        obj_val = obj_func(cor_vec)
+        return obj_val
+
+    # Loop over vaccination start times and durations
+    n_jobs = joblib.cpu_count(only_physical_cores=True)
+    print(f"Scanning over start times and durations using {n_jobs} cores")
+    results = list(
+        tqdm.tqdm(
+            joblib.Parallel(return_as="generator", n_jobs=n_jobs)(
+                joblib.delayed(worker)(start, duration)
+                for start, duration in itertools.product(
+                    start_time_grid_vals, duration_grid_vals
+                )
+            ),
+            total=len(start_time_grid_vals) * len(duration_grid_vals),
+        )
+    )
+    df_grid_search = (
+        pd.DataFrame(
+            np.reshape(
+                results,
+                (len(start_time_grid_vals), len(duration_grid_vals)),
+            ),
+            index=start_time_grid_vals,
+            columns=duration_grid_vals,
+            dtype=float,
+        )
+        .rename_axis("start", axis=0)
+        .rename_axis("duration", axis=1)
+    )
+    # Save the results
+    if save_path_grid_search is not None:
+        df_grid_search.to_csv(save_path_grid_search)
+    # Calculate optimal vaccination time range and corresponding case outbreak risk
+    start_time_best, duration_best = df_grid_search.stack().idxmin()
+    return start_time_best, duration_best
+
+
 if __name__ == "__main__":
     results_dir = pathlib.Path(__file__).parents[1] / "results/optimizing_vaccination"
     results_dir.mkdir(exist_ok=True, parents=True)
     run_analyses(
-        save_path=results_dir / "grid_search.csv",
+        save_path_grid_search=results_dir / "grid_search.csv",
         save_path_best=results_dir / "best.csv",
         save_path_vaccination_time_range_best=results_dir
         / "vaccination_time_range_best.csv",
         load_path_susceptibility_all_0=pathlib.Path(__file__).parents[1]
         / "results/susceptibility_all_0.csv",
+        grid_step=1,
     )
