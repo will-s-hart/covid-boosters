266d10263ddfab42a4247140d00a745712378f33
William Hart <william.hart@maths.ox.ac.uk>
2024-08-02 15:14:07 +0100

    Aesthetic changes

--- scripts/plotting/paper_figures.py
+++ scripts/plotting/paper_figures.py
@@ -103,8 +103,8 @@ def make_fig1():
         panel_dir / "without_vaccination/outbreak_risk.svg",
     ]
     template_path = FIGURE_DIR / "templates/fig1_template.svg"
-    panel_sz = (360, 364)
-    panel_offset = (-20, -45)
+    panel_sz = (340, 364)
+    panel_offset = (-15, -45)
     panel_positions = [
         (
             panel_offset[0] + panel_sz[0] * (i % 3),
@@ -112,8 +112,8 @@ def make_fig1():
         )
         for i in range(6)
     ]
-    panel_positions[3] = (160, 225)
-    panel_scalings = [1, 1, 1, 4 / 3, 1, 1]
+    panel_positions[3] = (190, 250)
+    panel_scalings = [1, 1, 1, 0.85 * 4 / 3, 1, 1]
     label_strings = [
         "A. Time-dependent transmission",
         "B. Heterogeneity in infectiousness",
@@ -148,7 +148,28 @@ def make_fig2():
         panel_dir / "vaccination_example/reproduction_number.svg",
         panel_dir / "vaccination_example/outbreak_risk.svg",
     ]
-    template_path = FIGURE_DIR / "templates/fig2_template.svg"
+    panel_sz = (350, 335)
+    panel_offset = (-12.5, DEFAULTS["panel_offset"][1])
+    make_figure(
+        save_path,
+        panel_paths,
+        panel_sz=panel_sz,
+        panel_offset=panel_offset,
+    )
+
+
+def make_fig2_alt():
+    save_path = FIGURE_DIR / "fig2_alt.svg"
+    panel_dir = pathlib.Path(__file__).parents[2] / "figures"
+    panel_paths = [
+        panel_dir / "within_host_dynamics/antibodies.svg",
+        panel_dir / "within_host_dynamics/susceptibility.svg",
+        panel_dir / "vaccination_example/susceptibility.svg",
+        panel_dir / "vaccination_example/unvaccinated_reproduction_number.svg",
+        panel_dir / "vaccination_example/reproduction_number.svg",
+        panel_dir / "vaccination_example/outbreak_risk.svg",
+    ]
+    template_path = FIGURE_DIR / "templates/fig2_alt_template.svg"
     panel_sz = (360, 340)
     panel_offset = (-12.5, DEFAULTS["panel_offset"][1])
     make_figure(
@@ -201,5 +222,6 @@ if __name__ == "__main__":
     FIGURE_DIR.mkdir(exist_ok=True, parents=True)
     make_fig1()
     make_fig2()
+    make_fig2_alt()
     make_fig3()
     make_fig4()
